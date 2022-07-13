b9f932a3bef8e9c11f3299f327f73bec1eaef09f
Nabeel Husain <nabeelhusain03@gmail.com>
2022-07-13 10:30:44 +0100

    Initial API request

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def get_data():  # put application's code here
+    response = requests.get("https://api-nile.tfl.gov.uk/StopPoint/490008660N/Arrivals").content
+
 
 
 if __name__ == '__main__':
